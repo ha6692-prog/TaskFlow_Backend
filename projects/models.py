a41694ea84a92e8bfaff8d49425c796976f03f10
ha6692-prog <ha6692@srmist.edu.in>
2026-06-27 15:54:22 +0530

    Created models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from workspaces.models import Workspace
 
-# Create your models here.
+
+class Project(models.Model):
+
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        related_name="projects"
+    )
+
+    name = models.CharField(
+        max_length=255
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
